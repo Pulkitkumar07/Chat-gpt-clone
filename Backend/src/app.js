4d994e09092034eefa008d01443cf82813f846a0
Pulkitkumar07 <pulkitchaudhary519@gmail.com>
2025-11-19 13:51:30 +0530

    Refactor server and frontend to standardize BASE_URL and improve socket server initialization

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -9,7 +9,7 @@ const app=express();
 app.use(express.json());
 app.use(cookieParser());
 app.use(cors({
-    origin:'https://chat-gpt-clone-hcp6.onrender.com/api',
+    origin: "https://chat-gpt-clone-hcp6.onrender.com",
     credentials:true
 }));
 
